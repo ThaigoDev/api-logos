3ac95e92ca92fe59d703b2f7d8da170328c10eef
ThaigoDev <thaigodeveloper@gmail.com>
2026-03-03 21:46:03 -0300

    first commit

---
+++ src/main/java/com/tech/logos/logos_api/LogosApiApplication.java
@@ -0,0 +1,13 @@
+package com.tech.logos.logos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/logos/logos_api/LogosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.logos.logos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
